15c802d801c954568279c5627d1789cd141bf46a
KangNansi <evillz@hotmail.com>
2017-04-13 10:10:33 +0200

    b

--- _minecraft/run/shaders/psbase.glsl
+++ _minecraft/run/shaders/psbase.glsl
@@ -1,19 +1,52 @@
 varying vec3 normal;
 varying vec3 vertex_to_light_vector;
+varying vec3 vertex_to_cam;
+varying vec3 worldPos;
 varying vec4 color;
 
 uniform float ambientLevel;
+uniform float elapsed;
+
+mat4 rotationMatrix(vec3 axis, float angle)
+{
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+    
+    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+                0.0,                                0.0,                                0.0,                                1.0);
+}
 
 void main()
 {
 	// Scaling The Input Vector To Length 1
+	//normal.x=cos(worldPos.x*2+elapsed);
+	//normal.y=sin(worldPos.y*4+elapsed);
 	vec3 normalized_normal = normalize(normal);
+	if(color.b>0.5){
+		normalized_normal.x += (cos(worldPos.x*3.+(elapsed*6.28))
+							+cos(worldPos.x*2.+(elapsed*6.28)/2.)
+							+cos(worldPos.x+(elapsed*6.28)/4.))/9.;
+		normalized_normal.y += (sin(worldPos.y*3.+(elapsed*6.28))
+							+sin(worldPos.y*2.+(elapsed*6.28)/2.)
+							+sin(worldPos.y+(elapsed*6.28)/4.))/9.;
+		normalized_normal = normalize(normalized_normal);
+	}
 	vec3 normalized_vertex_to_light_vector = normalize(vertex_to_light_vector);
-
+	vec3 normalized_half = normalize(vertex_to_light_vector+vertex_to_cam);
+	float specular = max(0,dot(normalized_half,normalized_normal));
 	// Calculating The Diffuse Term And Clamping It To [0;1]
-	float DiffuseTerm = clamp(dot(normal, vertex_to_light_vector), 0.0, 1.0);
+	float DiffuseTerm = clamp(dot(normalized_normal, vertex_to_light_vector), 0.0, 1.0);
 
 	// Calculating The Final Color
-	gl_FragColor.rgb = color.rgb * (DiffuseTerm*(1-ambientLevel) + ambientLevel) - gl_FragColor.rgb*(1-gl_FragColor.a);
+	color.rgb *= (0.5+specular/2.);
+	gl_FragColor.rgb = color.rgb;
+	gl_FragColor.r = (int)(color.r*20.)/20.;//specular;//color.rgb * (specular*DiffuseTerm*(1-ambientLevel) + ambientLevel) - gl_FragColor.rgb*(1-gl_FragColor.a);
+	gl_FragColor.g = (int)(color.g*20.)/20.;
+	gl_FragColor.b = (int)(color.b*20.)/20.;
+	
 	gl_FragColor.a = color.a;
 }
--- _minecraft/run/shaders/vsbase.glsl
+++ _minecraft/run/shaders/vsbase.glsl
@@ -1,6 +1,8 @@
 varying vec3 normal;
 varying vec3 vertex_to_light_vector;
+varying vec3 vertex_to_cam;
 varying vec4 color;
+varying vec3 worldPos;
 
 uniform float elapsed;
 uniform mat4 invertView;
@@ -11,14 +13,14 @@ void main()
 	//gl_Vertex.z += (sin((gl_Vertex.x)/20. + (elapsed*6.28)/40.)/10.0f) + cos((gl_Vertex.y)/20. + (elapsed*6.28)/40.)/10.0f;
 	
 	gl_Position = gl_ModelViewMatrix * gl_Vertex;
-	vec4 worldPos = invertView * gl_Position ;
-	vec3 vertex_to_cam = normalize(invertView*vec4(0,0,0,1)-worldPos);
+	worldPos = invertView * gl_Position ;
+	vertex_to_cam = normalize(invertView*vec4(0,0,0,1)-worldPos);
 	normal = gl_NormalMatrix*gl_Normal; 
-	if(gl_Color.b > 0.7){
-		gl_Position.z += (cos(worldPos.x*1.4 + (elapsed*6.28)/3.)-1+sin(worldPos.y*0.3 + (elapsed*6.28)/5.)-1+cos(worldPos.x*0.7 + (elapsed*6.28)/5.)-1)*0.9;
-		normal.x = sin(worldPos.x*0.7 + (elapsed*6.28)/5.);
+	if(gl_Color.b > 0.5){
+		gl_Position.z += (cos(worldPos.x*0.2 + (elapsed*6.28)/3.)-1+sin(worldPos.y*0.3 + (elapsed*6.28)/5.)-1+cos(worldPos.x*0.7 + (elapsed*6.28)/5.)-1)*0.9;
+		normal.x = sin(worldPos.x*0.2 + (elapsed*6.28)/3.)/2.;
 		normal.z = 1;
-		normal.y = -cos(worldPos.y*0.3 + (elapsed*6.28)/5.)/4.;
+		normal.y = -cos(worldPos.y*0.3 + (elapsed*6.28)/5.)/2.;
 		normal = normalize(normal);
 	}
 	gl_Position = gl_ProjectionMatrix * gl_Position;
@@ -29,11 +31,11 @@ void main()
 	//Direction lumiere
 	vertex_to_light_vector = normalize(invertView*gl_LightSource[0].position);
 
-	float specular = max(0,dot(normalize(vertex_to_light_vector+vertex_to_cam),normal));
-	if(specular<0.95) specular = 0;
-	else specular = (specular-0.95)/0.05;
+	float specular = max(0,dot(normalize(vertex_to_light_vector+vertex_to_cam),gl_Normal));
+
 
 	//Couleur
-	color = gl_Color+specular*0.6;//+specular;//*max(0,dot(vertex_to_light_vector, normal));
+	color.rgb = gl_Color.rgb;//*max(0,dot(vertex_to_light_vector, normal));
+	color.a = gl_Color.a;
 	//normal = normal;
 }
